011c390f4bf0f036a06f347c2a47ee78df77ceb0
Arief Hilmi <arief.hilmi@gmail.com>
2018-10-04 09:18:40 +0800

    reupdate codes
    use phpmailer

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form method="post" action="email_zend.php">
+<form method="post" action="email.php">
   Email: <input name="email" id="email" type="text" /><br /><br/>
   
   <!--CC: <input name="cc_email" id="cc_email" type="text" /><br /><br/>-->
